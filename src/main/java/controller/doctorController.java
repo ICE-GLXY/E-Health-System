85b11241488c87c009094e78c524465456534ff3
Zaeem182 <zaeempetersen@gmail.com>
2023-11-16 22:07:56 +0200

    controllers completed

---
+++ src/main/java/controller/doctorController.java
@@ -0,0 +1,66 @@
+package controller;
+
+import domain.doctor;
+import domain.pharmacist;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import service.IDoctorService;
+
+import javax.print.Doc;
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("E-Health-System/doctor")
+@Slf4j
+public class doctorController {
+
+    private final IDoctorService doctorService;
+
+    @Autowired
+    public doctorController(IDoctorService doctorService){
+        this.doctorService = doctorService;
+    }
+
+    //save
+    @PostMapping("save")
+    public ResponseEntity<doctor> save(@Valid @RequestBody doctor doctor){
+        log.info("save request:{}", doctor);
+        doctor save = doctorService.save(doctor);
+        return ResponseEntity.ok(save);
+    }
+
+    //read
+    @GetMapping("read/{doctorID}")
+    public ResponseEntity<Optional<doctor>> read (@PathVariable String doctorID){
+        log.info("read request:{}", doctorID);
+
+        try {
+            Optional<doctor> read = doctorService.read(doctorID);
+            return ResponseEntity.ok(read);
+
+        }catch(IllegalArgumentException e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,e.getMessage());
+        }
+    }
+
+    //Delete
+    @DeleteMapping("delete/{doctor}")
+    public ResponseEntity<doctor> delete (@PathVariable doctor doctor){
+        log.info("read request:{}", doctor);
+        this.doctorService.delete(doctor);
+        return ResponseEntity.noContent().build();
+    }
+
+    //Get all
+    @GetMapping("all")
+    public ResponseEntity<List<doctor>>findAll(){
+        List<doctor>doctorList = this.doctorService.readAll();
+        return ResponseEntity.ok(doctorList);
+    }
+}
